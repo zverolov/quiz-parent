aed7451638f0158ab7c925802b3683cda491fc80
Vikt0r <159viktor159@gmail.com>
2017-02-13 15:52:34 +0300

    Initial commit

---
+++ quiz-admin/inside/src/main/java/InsideClass.java
@@ -0,0 +1,8 @@
+/**
+ * Created by sw0rd on 13.02.17.
+ */
+public class InsideClass {
+    public static void main(String[] args) {
+        System.out.println("this is inside project");
+    }
+}
---
+++ quiz-admin/src/main/java/NewClass.java
@@ -0,0 +1,8 @@
+/**
+ * Created by sw0rd on 13.02.17.
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
